a556591b46faeb9f35455b509a724db41dedc323
minsara29 <68796445+minsara29@users.noreply.github.com>
2021-01-25 21:02:03 -0600

    Shiny number

--- shiny.py
+++ shiny.py
@@ -1,15 +1,22 @@
 def isit_order(n):
-  l = str(n)
-  return all(l[i] <= l[i+1] for i in range(len(l)-1))
+    """
+    A shiny number is a number where each base-10 digit (from left to right)
+    is ≤ the next digit
+    :param n: 
+    :return:True
+    """
+    l = str(n)
+    return all(l[i] <= l[i+1] for i in range(len(l)-1))
 
 
 n = int(input("Print the Number:"))
 c = 0 
 while True:
-  shiny = isit_order(n)
-  if shiny == True:
-    print(f" yes I am Order {n}"  )
-    c += 1 
-    if c == 100:
-      break
-  n += 1
+    shiny = isit_order(n)
+    if shiny == True:
+        print(f"{n}")
+        c += 1
+        if c == 100:
+            break
+    n += 1
+
